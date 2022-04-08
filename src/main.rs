c957c7958443265154c610562a0bf0c5630d80ec
CrispiCas <ole.siemens@gmx.net>
2022-04-08 20:11:49 +0200

    Kelvin stuff

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 use clap::Parser;
 
-#[derive(Parser ,Debug)]
+#[derive(Parser, Debug)]
 #[clap(version, author = "Crispr", about = "A simple cli calculator")]
 
 struct Args{
-    /// The temperature in celsius which should be converted to Fahrenheit
-    #[clap(short, long)]
-    temp: u16,
+    /// The temperature in celcius which should be converted to fahrenheit or kelvin
+    celsius: f64,
+    /// The unit to convert to. Can be either fahrenheit (f) or kelvin (k)
+    unit: char,
 }
 
 fn main() {
     let args = Args::parse();
+    
+    match args.unit {
+        'k' => {
+            let kelvin = args.celsius - 273.15;
+            println!("The temperature in kelvin is {}°", kelvin);
+        },
 
-    let temp = (args.temp * 9/5) + 32;
-    println!("The temprature in Fahrenheit is {}°", temp);
+        'f' => {
+            let fahrenheit = (args.celsius * 9.0/5.0) + 32.0;
+            println!("The temperature in fahrenheit is {}°", fahrenheit);
+        },
+
+        _ => eprintln!("Pleae input the unit. 'k' for kelvin, 'f' for fahrenheit"),
+    };
+
+
 }
